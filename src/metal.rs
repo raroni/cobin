09a32f8672fc8420eb2cfb5628f39375fee32288
Rasmus Rønn Nielsen <rasmus@polylab.dk>
2022-11-13 15:41:24 +0100

    Add a few methods/types related to Metal blending.

--- src/metal.rs
+++ src/metal.rs
@@ -435,6 +435,42 @@ pub enum MTLGPUFamily {
   MAC_CATALYST2 = 4002
 }
 
+#[repr(u64)]
+#[allow(non_camel_case_types)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum MTLBlendFactor {
+    Zero = 0,
+    One = 1,
+    SourceColor = 2,
+    OneMinusSourceColor = 3,
+    SourceAlpha = 4,
+    OneMinusSourceAlpha = 5,
+    DestinationColor = 6,
+    OneMinusDestinationColor = 7,
+    DestinationAlpha = 8,
+    OneMinusDestinationAlpha = 9,
+    SourceAlphaSaturated = 10,
+    BlendColor = 11,
+    OneMinusBlendColor = 12,
+    BlendAlpha = 13,
+    OneMinusBlendAlpha = 14,
+    Source1Color = 15,
+    OneMinusSource1Color = 16,
+    Source1Alpha = 17,
+    OneMinusSource1Alpha = 18,
+}
+
+#[repr(u64)]
+#[allow(non_camel_case_types)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum MTLBlendOperation {
+    Add = 0,
+    Subtract = 1,
+    ReverseSubtract = 2,
+    Min = 3,
+    Max = 4,
+}
+
 pub struct MTLCommandBufferHandler {
   pub block: block::RcBlock<(*mut MTLCommandBuffer, ), ()>
 }
--- src/metal/mtl_render_pipeline_color_attachment_descriptor.rs
+++ src/metal/mtl_render_pipeline_color_attachment_descriptor.rs
@@ -18,4 +18,28 @@ impl MTLRenderPipelineColorAttachmentDescriptor {
   pub unsafe fn set_pixel_format(&self, pixel_format: MTLPixelFormat) {
     msg_send![self, setPixelFormat: pixel_format]
   }
+
+  pub unsafe fn set_alpha_blend_operation(&self, operation: MTLBlendOperation) {
+    msg_send![self, setAlphaBlendOperation:operation]
+  }
+
+  pub unsafe fn set_source_rgb_blend_factor(&self, factor: MTLBlendFactor) {
+    msg_send![self, setSourceRGBBlendFactor:factor]
+  }
+
+  pub unsafe fn set_destination_rgb_blend_factor(&self, factor: MTLBlendFactor) {
+    msg_send![self, setDestinationRGBBlendFactor:factor]
+  }
+
+  pub unsafe fn set_source_alpha_blend_factor(&self, factor: MTLBlendFactor) {
+    msg_send![self, setSourceAlphaBlendFactor:factor]
+  }
+
+  pub unsafe fn set_destination_alpha_blend_factor(&self, factor: MTLBlendFactor) {
+    msg_send![self, setDestinationAlphaBlendFactor:factor]
+  }
+
+  pub unsafe fn set_blending_enabled(&self, enabled: bool) {
+    msg_send![self, setBlendingEnabled:enabled]
+  }
 }
